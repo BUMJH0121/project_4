dfc1d83e148334b5c71af36605b5b6600b761fde
BUMJH0121 <bumjh0121@gmail.com>
2021-04-19 17:51:16 +0900

    modify user_input url

--- Data_Processing_AWS/app.py
+++ Data_Processing_AWS/app.py
@@ -41,6 +41,12 @@ def tmp():
     data = requests.get('http://54.156.131.155:5000/data/bus_location')
     return render_template("convert.html", value = jsonify(data))
 
+@app.route('/user_input', methods=['POST'])
+def user_input():
+    if request.method == 'POST':
+        res_json = json.loads(request.data)
+    return res_json
+
 
 
 if __name__ == "__main__":
