2ba72bede8452924928045c75d27b6b04286d35c
EC2 Default User <ec2-user@ip-172-31-16-117.ec2.internal>
2021-04-19 02:49:28 +0000

    DONE USER_UI_MODIFY

--- User_ui_inAWS/user_ui.py
+++ User_ui_inAWS/user_ui.py
@@ -11,7 +11,7 @@ def home():
         value = json.dumps(request.form.to_dict(flat=False))
         print(type(value), value)
         requests.post("http://54.156.131.155:5000/", data = value)
-        res = requests.get("http://54.156.131.155:5000")
+        res = requests.get("http://54.156.131.155:5000/temp")
         data = json.loads(res.text)
         return render_template('result.html', data = data)
     return render_template('index.html', region_json_gu= set(convert_gu), region_json_dong= region_dict)
@@ -31,4 +31,4 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
