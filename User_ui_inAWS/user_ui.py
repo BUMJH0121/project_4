ab7587e9bb0a171e02bf92daad8458a6dcefbcef
BUMJH0121 <bumjh0121@gmail.com>
2021-04-20 09:41:49 +0900

    modify word:

--- User_ui_inAWS/user_ui.py
+++ User_ui_inAWS/user_ui.py
@@ -10,7 +10,7 @@ def home():
     if request.method == 'POST':
         value = json.dumps(request.form.to_dict(flat=False))
         requests.post("http://54.156.131.155:5000/user_input", data = value)
-        res = requests.get("http://54.156.131.155:5000/temp")
+        res = requests.get("http://54.156.131.155:5000/user_input")
         data = json.loads(res.text)
         return render_template('result.html', data = data)
     return render_template('index.html', region_json_gu= set(convert_gu), region_json_dong= region_dict, service_json= service_name)
