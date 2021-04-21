62bb6b0633b7ab0af84e0ba494ed132d30c2a2cd
BUMJH0121 <bumjh0121@gmail.com>
2021-04-21 23:19:33 +0900

    real sum

--- User_ui_inAWS/user_ui.py
+++ User_ui_inAWS/user_ui.py
@@ -12,7 +12,7 @@ def home():
         res =requests.post("http://52.73.176.237:5000/user_input", data = value)
         data = json.loads(res.text)
         return render_template('result.html', data = data)
-    return render_template('index.html', region_json_gu= region_name_gu, region_json_dong= region_dict, service_json=service_name, bus_stop = bus_stop)
+    return render_template('index.html', region_json_gu= region_name_gu, region_json_dong= region_dict, service_json=service_name)
     # service_json=, year_json=
 
 # @app.route('/data')
