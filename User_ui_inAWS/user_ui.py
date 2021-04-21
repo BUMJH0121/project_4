23d5504245ddc62e227efe8d1f6bc6a34a7f7bf7
EC2 Default User <ec2-user@ip-172-31-16-117.ec2.internal>
2021-04-21 01:53:49 +0000

    modify

--- User_ui_inAWS/user_ui.py
+++ User_ui_inAWS/user_ui.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def home():
     if request.method == 'POST':
         value = json.dumps(request.form.to_dict(flat=False), ensure_ascii=False).encode('utf-8')
-        res =requests.post("http://34.227.101.182:5000/user_input", data = value)
+        res =requests.post("http://52.73.176.237:5000/user_input", data = value)
         data = json.loads(res.text)
         return render_template('result.html', data = data)
     return render_template('index.html', region_json_gu= set(convert_gu), region_json_dong= region_dict, service_json= service_name)
