92881ee2a03581f589e7726e0b288bbe3936cc12
BUMJH0121 <bumjh0121@gmail.com>
2021-04-22 15:35:17 +0900

    check

--- User_ui_inAWS/user_ui.py
+++ User_ui_inAWS/user_ui.py
@@ -11,6 +11,7 @@ def home():
         value = json.dumps(request.form.to_dict(flat=False), ensure_ascii=False).encode('utf-8')
         res =requests.post("http://52.73.176.237:5000/user_input", data = value)
         data = json.loads(res.text)
+        print(data)
         return render_template('result.html', data = data)
     return render_template('index.html', region_json_gu= region_name_gu, region_json_dong= region_dict, service_json=service_name)
     # service_json=, year_json=
