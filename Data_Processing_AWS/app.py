d7a780cec14e2ca7d3ff148d4ec776bb6da7563a
BUMJH0121 <bumjh0121@gmail.com>
2021-04-22 16:07:16 +0900

    tset

--- Data_Processing_AWS/app.py
+++ Data_Processing_AWS/app.py
@@ -77,7 +77,7 @@ def user_input():
         res3 = requests.get(url3)
         data3 = res3.json()
         code_df = pd.DataFrame(data3)
-        code = code_df[code_df['dong'] == f'res_json["region_dong"][0]']['code'].drop.duplicates()
+        code = code_df[code_df['dong'] == f'res_json["region_dong"][0]']['code'].drop_duplicates()
 
         requests.post('http://18.206.167.14:20000/user', data = code)
 
