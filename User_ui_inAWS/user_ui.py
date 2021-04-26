962dedfc2de11dd099aee261dcfb71212e7ae2fe
BUMJH0121 <bumjh0121@gmail.com>
2021-04-26 16:03:26 +0900

    done user_ui

--- User_ui_inAWS/user_ui.py
+++ User_ui_inAWS/user_ui.py
@@ -14,19 +14,6 @@ def home():
         data = json.loads(res.text)
         return render_template('result.html', data = data)
     return render_template('index.html', region_json_gu= region_name_gu, region_json_dong= region_dict, service_json=service_name)
-    # service_json=, year_json=
-
-# @app.route('/data')
-# def get_data():
-#     return 'HI'
-
-# @app.route('/mongo')
-# def mongoTest():
-#     client = MongoClient('mongodb://localhost:27017/')
-#     db = client.test
-#     collection = db.seoul
-#     cursor = collection.find()
-#     return dumps(cursor, ensure_ascii=False)
 
 
 if __name__ == '__main__':
